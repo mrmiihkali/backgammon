c6ceaa15f9aa378318d8355c1296ce5123393c35
Miika Lappalainen <miika@foodie.fm>
2016-12-22 11:00:24 +0200

    More functionality. Slightly better code.

--- board_renderer_svg.js
+++ board_renderer_svg.js
@@ -3,198 +3,231 @@
 //********************************************************
 // Board
 
-function Board() {
-  this.init = function(container, gameSate) {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }  
+function Board() {}
 
-    this.gameState = gameState
-    this.container = container
-    this.pips = []
-    this.board_rect = container.getBoundingClientRect();
-
-    w = this.board_rect.width / 13
-    h = (this.board_rect.height / 12) * 5
-
-    this.max_stack = 5
-
-    this.checker_size = h / this.max_stack
-    this.checker_offset = (w - this.checker_size) / 2
-    this.die_size = this.board_rect.height - 2 * h - 10
-    
-    this.create_pips()
-    this.create_dice()
-  }
-
-  this.create_pips = function() {
-    w = this.board_rect.width / 13
-    h = (this.board_rect.height / 12) * 5
-    y = (this.board_rect.height / 12) * 7
-    x = this.board_rect.width
-
-    black = true
-
-    pipnum = 0  
-    for (var i = 0; i < 13; i ++, pipnum++) {
-      x -= w
-      if (i == 6) {
-        pipnum--
-        continue
-      }
-      var pip = new Pip(board, x, y, w, h, pipnum, black)
-      e = document.createElement("svg")
-      pip.bind(e)
-      this.container.appendChild(e)
-      this.pips.push(pip)
-      black = !black
-    }
-    
-    black = true
-    y = 0
-    x = -w
-    pipnum = 12
-    for (var i = 0; i < 13; i++, pipnum++) {
-      x += w
-      if (i == 6) {
-        pipnum--
-        continue
-      }
-      var pip = new Pip(board, x, y, w, h, pipnum, black)
-      e = document.createElement("svg")
-      pip.bind(e)
-      this.container.appendChild(e)
-      this.pips.push(pip)
-      black = !black
-    }
-  }
-
-  this.create_dice = function() {
-    y = this.board_rect.height / 2
-    y -= this.die_size / 2
-
-    this.dice = []
-    this.dice[0] = new Die(30, y, this.die_size, true)
-    this.dice[1] = new Die(30 + 5 + this.die_size, y, this.die_size, true)
-    this.dice[2] = new Die(this.board_rect.width - 35 - 2 * this.die_size, y, this.die_size, false)
-    this.dice[3] = new Die(this.board_rect.width - 30 - this.die_size, y, this.die_size, false)
-
-    this.dice.forEach((function(die) {
-      e = document.createElement("svg")
-      die.bind(e)
-      this.container.appendChild(e)
-    }).bind(this))
-  }
-
-  this.update_game_state = function() {
-    for (var i = 0; i < 24; i++) {
-      this.pips[i].set_checkers(Math.abs(gameState.checkers[i]), gameState.checkers[i] < 0)
-    }
-
-    if (this.gameState.on_turn() == 0) {
-      this.dice[0].element.style.visibility = 'hidden'
-      this.dice[1].element.style.visibility = 'hidden'
-      this.dice[2].element.style.visibility = 'visible'
-      this.dice[3].element.style.visibility = 'visible'
-
-      this.dice[2].value = this.gameState.dice[0]
-      this.dice[3].value = this.gameState.dice[1]
-
-      this.dice[2].render()
-      this.dice[3].render()
-    }
-    else {
-      this.dice[2].element.style.visibility = 'hidden'
-      this.dice[3].element.style.visibility = 'hidden'
-      this.dice[0].element.style.visibility = 'visible'
-      this.dice[1].element.style.visibility = 'visible'
-
-      this.dice[0].value = this.gameState.dice[0]
-      this.dice[1].value = this.gameState.dice[1]
-
-      this.dice[0].render()
-      this.dice[1].render()
-    }
-  }
-
-  this.set_pip_click_handler = function(handler) {
-    this.pip_click_handler = handler
-
-    this.pips.forEach(function(pip) {
-      pip.element.onclick = handler
-    })
-  }
-
-  this.set_dice_click_handler = function(handler) {
-    this.dice_click_handler = handler
-
-    this.dice.forEach(function(die) {
-      die.element.onclick = handler
-    })
-  }
-
-  this.move_checker = function(from, to, game, transitionCb) {
-    from_pip = this.pips[from]
-    to_pip = this.pips[to]
-
-    if (from_pip.checkers[from] == 0)
-      return
-
-    from_checker = from_pip.checkers.pop()
-
-    from_pip.set_checkers(from_pip.checkers.length, from_checker.style == 'black')
-
-    start_pos = this.checker_pos_on_board(from_pip.pip, from_pip.checkers.length)
-    element = document.createElement('svg')
-    from_checker.x = start_pos.x + this.checker_offset
-    from_checker.y = start_pos.y
-    from_checker.bind(element)
-    
-    this.container.appendChild(element)
-
-    to_pos = this.checker_pos_on_board(to_pip.pip, Math.abs(game.checkers[to]) - 1)
-    
-    var context = {board: this, 
-      to_pip: to_pip, 
-      to_pos: to_pos, 
-      element: element,
-      from_checker: from_checker,
-      callback: transitionCb
-    }
-    setTimeout(this.start_checker_transition.bind(context), 1)
+Board.prototype.init = function(container, gameSate) {
+  while (container.firstChild) {
+      container.removeChild(container.firstChild);
   }  
 
-  this.start_checker_transition = function() {
-    this.element.style.transition = '1s'
-    this.element.style.left = to_pos.x + this.board.checker_offset + 'px'
-    this.element.style.top = to_pos.y + 'px'
-    this.element.event_context = this
-    
-    this.element.addEventListener("transitionend", this.board.finalize_checker_transition)
-  }
+  this.gameState = gameState
+  this.container = container
+  this.pips = []
+  this.board_rect = container.getBoundingClientRect();
 
-  this.finalize_checker_transition = function(event) {
-    if (event.propertyName != 'left')
-      return
-    context = event.currentTarget.event_context
-    context.to_pip.set_checkers(context.to_pip.checkers.length + 1, context.from_checker.style == 'black')
-    context.board.container.removeChild(context.element)
-    context.callback.call()
-  }
+  w = this.board_rect.width / 13
+  h = (this.board_rect.height / 12) * 5
 
-  this.checker_pos_on_board = function(pip, numCheckers) {
-    h = this.board_rect.height / 12
+  this.max_stack = 5
 
-    x = this.pips[pip].x
-    if (pip < 12) {
-      y = this.board_rect.height - (numCheckers + 1) * h
+  this.checker_size = h / this.max_stack
+  this.checker_offset = (w - this.checker_size) / 2
+  this.die_size = this.board_rect.height - 2 * h - 10
+  
+  this.create_pips()
+  this.create_dice()
+}
+
+Board.prototype.create_pips = function() {
+  w = this.board_rect.width / 13
+  h = (this.board_rect.height / 12) * 5
+  y = (this.board_rect.height / 12) * 7
+  x = this.board_rect.width
+
+  black = true
+
+  pipnum = 0  
+  for (var i = 0; i < 13; i ++, pipnum++) {
+    x -= w
+    if (i == 6) {
+      pipnum--
+      continue
     }
-    else {
-      y = numCheckers * h
-    }
-
-    return {x: x, y: y}
+    var pip = new Pip(board, x, y, w, h, pipnum, black, 'normal')
+    e = document.createElement("svg")
+    pip.bind(e)
+    this.container.appendChild(e)
+    this.pips.push(pip)
+    black = !black
   }
+  
+  black = true
+  y = 0
+  x = -w
+  pipnum = 12
+  for (var i = 0; i < 13; i++, pipnum++) {
+    x += w
+    if (i == 6) {
+      pipnum--
+      continue
+    }
+    var pip = new Pip(board, x, y, w, h, pipnum, black, 'normal')
+    e = document.createElement("svg")
+    pip.bind(e)
+    this.container.appendChild(e)
+    this.pips.push(pip)
+    black = !black
+  }
+
+  // White bar
+  y = this.board_rect.height / 2
+  x = w * 6
+  var pip = new Pip(board, x, y, w, h, 24, black, 'bar')
+  e = document.createElement("svg")
+  pip.bind(e)
+  this.container.appendChild(e)
+  this.pips.push(pip)
+
+  // Black bar
+  y = 0
+  var pip = new Pip(board, x, y, w, h, 25, black, 'bar')
+  e = document.createElement("svg")
+  pip.bind(e)
+  this.container.appendChild(e)
+  this.pips.push(pip)
+
+}
+
+Board.prototype.create_dice = function() {
+  y = this.board_rect.height / 2
+  y -= this.die_size / 2
+
+  this.dice = []
+  this.dice[0] = new Die(30, y, this.die_size, true)
+  this.dice[1] = new Die(30 + 5 + this.die_size, y, this.die_size, true)
+  this.dice[2] = new Die(this.board_rect.width - 35 - 2 * this.die_size, y, this.die_size, false)
+  this.dice[3] = new Die(this.board_rect.width - 30 - this.die_size, y, this.die_size, false)
+
+  this.dice.forEach((function(die) {
+    e = document.createElement("svg")
+    die.bind(e)
+    this.container.appendChild(e)
+  }).bind(this))
+}
+
+Board.prototype.update_game_state = function() {
+  for (var i = 0; i < 26; i++) {
+    this.pips[i].set_checkers(Math.abs(gameState.checkers[i]), gameState.checkers[i] < 0)
+  }
+
+  if (this.gameState.on_turn() == 0) {
+    this.dice[0].element.style.visibility = 'hidden'
+    this.dice[1].element.style.visibility = 'hidden'
+    this.dice[2].element.style.visibility = 'visible'
+    this.dice[3].element.style.visibility = 'visible'
+
+    this.dice[2].value = this.gameState.dice[0]
+    this.dice[3].value = this.gameState.dice[1]
+
+    this.dice[2].render()
+    this.dice[3].render()
+  }
+  else {
+    this.dice[2].element.style.visibility = 'hidden'
+    this.dice[3].element.style.visibility = 'hidden'
+    this.dice[0].element.style.visibility = 'visible'
+    this.dice[1].element.style.visibility = 'visible'
+
+    this.dice[0].value = this.gameState.dice[0]
+    this.dice[1].value = this.gameState.dice[1]
+
+    this.dice[0].render()
+    this.dice[1].render()
+  }
+}
+
+Board.prototype.set_pip_click_handler = function(handler) {
+  this.pip_click_handler = handler
+
+  this.pips.forEach(function(pip) {
+    pip.element.onclick = handler
+  })
+}
+
+Board.prototype.set_dice_click_handler = function(handler) {
+  this.dice_click_handler = handler
+
+  this.dice.forEach(function(die) {
+    die.element.onclick = handler
+  })
+}
+
+Board.prototype.move_checker = function(from, to, game, transitionCb) {
+  from_pip = this.pips[from]
+  to_pip = this.pips[to]
+
+  if (from_pip.checkers.length == 0) {
+    return
+  }  
+  from_checker = from_pip.checkers.pop()
+
+  from_pip.set_checkers(from_pip.checkers.length, from_checker.style == 'black')
+
+  start_pos = this.checker_pos_on_board(from_pip.pip, from_pip.checkers.length)
+  element = document.createElement('svg')
+  from_checker.x = start_pos.x + this.checker_offset
+  from_checker.y = start_pos.y
+  from_checker.bind(element)
+  
+  this.container.appendChild(element)
+
+  to_pos = this.checker_pos_on_board(to_pip.pip, Math.abs(game.checkers[to]) - 1)
+  
+  var context = {board: this, 
+    to_pip: to_pip, 
+    to_pos: to_pos, 
+    element: element,
+    from_checker: from_checker,
+    callback: transitionCb
+  }
+  
+  setTimeout(this.start_checker_transition.bind(context), 100)
+}  
+
+Board.prototype.start_checker_transition = function() {
+  this.element.addEventListener("transitionend", this.board.finalize_checker_transition)
+  this.element.style.transition = 'left 0.5s, top 0.5s'
+  oldleft = this.element.style.left
+  oldtop = this.element.style.top
+  this.element.style.left = to_pos.x + this.board.checker_offset + 'px'
+  this.element.style.top = to_pos.y + 'px'
+  this.element.event_context = this
+  this.element.transition_count = 0
+  if (oldleft != this.element.style.left)
+    this.element.transition_count++
+  if (oldtop != this.element.style.top)
+    this.element.transition_count++
+}
+
+Board.prototype.finalize_checker_transition = function(event) {
+  event.currentTarget.transition_count--;
+  if (event.currentTarget.transition_count != 0)
+    return
+  context = event.currentTarget.event_context
+  context.to_pip.set_checkers(context.to_pip.checkers.length + 1, context.from_checker.style == 'black')
+  context.board.container.removeChild(context.element)
+  setTimeout(context.callback.bind(context), 100)
+}
+
+Board.prototype.checker_pos_on_board = function(pip, numCheckers) {
+  h = this.board_rect.height / 12
+
+  pip = this.pips[pip]
+  x = pip.x
+  if (pip.pip < 12) {
+    y = pip.y + pip.h - (numCheckers + 1) * h
+  }
+  else if (pip.pip < 24) {
+    y = numCheckers * h
+  }
+  else if (pip.pip == 24) {
+    y = pip.y + (numCheckers) * h 
+  }
+  else if (pip.pip == 25) {
+    y = pip.y + pip.h - (numCheckers + 1) * h
+  }
+
+  return {x: x, y: y}
 }
 
 //********************************************************
@@ -202,15 +235,19 @@ function Board() {
 
 Pip.prototype = Object.create(BoardElement.prototype)
 
-function Pip(board, x, y, w, h, pip, black) {
+function Pip(board, x, y, w, h, pip, black, type) {
   BoardElement.call(this, x, y, w, h)
 
+  this.type = type
   this.board = board
   this.pip = pip
   this.style = black ? 'black' : 'white'
   this.checkers = []
+}
 
-  this.render = function() {
+Pip.prototype.render = function() {
+  svg = ''
+  if (this.type == 'normal') {
     var points = ""
     if (this.pip > 11) {
       points += 0 + ',' + 0 + ' ' + 
@@ -226,22 +263,30 @@ function Pip(board, x, y, w, h, pip, black) {
     svg = '<svg class="pip" data-style="' + this.style + '" width="' + this.w + '" height="' + this.h + '"> \
             <polygon points="' + points + '"/> \
           </svg>'
-    
-    this.element.innerHTML = svg
+  }
+  else if (this.type == 'bar') {
+
+  }
+  else { // bore-off
+
   }
 
-  this.set_checkers = function(num, black) {
-    while (this.element.firstChild) {
-        this.element.removeChild(this.element.firstChild);
-    }
+  this.element.innerHTML = svg
+}
 
-    this.render()
+Pip.prototype.set_checkers = function(num, black) {
+  while (this.element.firstChild) {
+      this.element.removeChild(this.element.firstChild);
+  }
 
-    this.checkers = []
+  this.render()
 
+  this.checkers = []
+
+  if (this.type == 'normal' || this.type == 'bar') {
     y = this.h - this.board.checker_size
     dir = -this.board.checker_size
-    if (this.pip > 11) {
+    if ((this.pip > 11 && this.pip < 24) || this.pip == 24) {
       dir = this.board.checker_size
       y = 0
     }
@@ -256,8 +301,10 @@ function Pip(board, x, y, w, h, pip, black) {
       y += dir
     }
   }
-
 }
+
+//********************************************************
+// Checker
 
 Checker.prototype = Object.create(BoardElement.prototype)
 
@@ -267,15 +314,17 @@ function Checker(x, y, w, pip, black) {
   this.pip = pip
   this.style = black ? 'black' : 'white'
 
-  this.render = function() {
-
-    svg = '<svg class="checker" data-style="' + this.style + '" width="' + this.w + '" height="' + this.h + '"> \
-            <circle cx="' + (this.w / 2) + '" cy="' + (this.w / 2) + '" r="' + (this.w / 2) + '"/> \
-           </svg>'
-
-    this.element.innerHTML = svg   
-  }
 }
+
+Checker.prototype.render = function() {
+
+  svg = '<svg class="checker" data-style="' + this.style + '" width="' + this.w + '" height="' + this.h + '"> \
+          <circle cx="' + (this.w / 2) + '" cy="' + (this.w / 2) + '" r="' + (this.w / 2) + '"/> \
+         </svg>'
+
+  this.element.innerHTML = svg   
+}
+
 
 //********************************************************
 // Die
--- gamestate.js
+++ gamestate.js
@@ -1,75 +1,238 @@
 
 GameState = function() {
-
-  this.reset = function() {
-    this.checkers = new Array(27).fill(0)
-
-    this.checkers[0] = -2
-    this.checkers[5] = 5
-    this.checkers[7] = 3
-    this.checkers[11] = -5
-    this.checkers[12] = 5
-    this.checkers[16] = -3
-    this.checkers[18] = -5
-    this.checkers[23] = 2
-
-    this.dice = []
-    this.roll()
-
-    this.player = 0
-    this.dice_moved = 0
-  }
-
-  this.moved = function() {
-    if (this.dice_moved == 2)
-      return
-    this.dice_moved += 1
-  }
-
-  this.on_turn = function() {
-    return this.player
-  }
-
-  this.change_player = function() {
-    this.player = this.player == 0 ? 1 : 0
-    return this.player
-  }
-
-  this.to_move = function() {
-    if (this.dice_moved > 1)
-      return -1;
-    return this.dice[this.dice_moved]
-  }
-
-  this.roll = function() {
-    die1 = Math.floor(Math.random() * 6) + 1
-    die2 = Math.floor(Math.random() * 6) + 1
-
-    this.dice[0] = Math.max(die1, die2)
-    this.dice[1] = Math.min(die1, die2)
-    this.dice_moved = 0
-  }
-
-  this.move = function(from, to, player) {
-    if (player == 0) {
-      dir = 1
-      if (this.checkers[from] < 1 || this.checkers[to] < 0) {
-        return false
-      }
-    }
-    else {
-      dir = -1
-      if (this.checkers[from] > 0 || this.checkers[to] > 0) {
-        return false
-      }
-    }
-
-    this.checkers[from] -= dir
-    this.checkers[to] += dir
-
-    return true
-  }
-
   this.reset()
-
 }
+
+GameState.prototype.reset = function() {
+  // 0-5 = white home, 18-23 = black home
+  // pip[24] = black bar, pip[25] = white bar, pip[26] = white off, pip[27] = black off
+  // white checkers = positive, black checkers = negative
+  // player0 = white, player1 = black
+  this.checkers = new Array(28).fill(0)
+
+  this.checkers[0] = -2
+  this.checkers[5] = 5
+  this.checkers[7] = 3
+  this.checkers[11] = -5
+  this.checkers[12] = 5
+  this.checkers[16] = -3
+  this.checkers[18] = -5
+  this.checkers[23] = 2
+
+  this.dice = []
+  this.roll()
+
+  this.player = 0
+  this.dice_moved = 0
+}
+
+GameState.prototype.allowed_moves = function(from) {
+  var player = this.on_turn()
+
+  var moves = []
+  for (d in this.dice) {
+    var to = this.to_pip(from, this.dice[d], player)
+    var allowed = this.allowed_move(from, to, player)
+    moves.push({to: to, allowed: allowed})
+  }
+
+  return moves
+}
+
+GameState.prototype.allowed_move = function(from, to, player) {
+  // Make more strict; check also player...
+  if (from < 0 || from > 27 || to < 0 || to > 27) 
+    return false
+
+  var checkers_from = this.checkers[from]
+  if (checkers_from == 0)
+    return false
+
+  // Is there checkers to move?
+  if ((checkers_from < 0 && player == 0) ||
+      (checkers_from > 0 && player == 1))
+    return false
+
+  var checkers_to = this.checkers[to]
+  // Is there block?
+  if ((checkers_to < -1 && player == 0) ||
+      (checkers_to > 1 && player == 1))
+    return false
+
+  // Sanity; player0 should never be on player1 bar
+  if ((from == 24 && player == 1 ||
+      (from == 25 && player == 0))) {
+    throw "Invalid gamestate!"
+    return false
+  }
+
+  var player_in_home = true
+  if (player == 0) {
+    for (var i = 23; i >= 0; i--) {
+      if (this.checkers[i] > 0) {
+        player_in_home = false
+        break
+      }
+    }
+  }
+  else {
+    for (var i = 0; i < 24; i++) {
+      if (this.checkers[i] < 0) {
+        player_in_home = false
+        break
+      }
+    }
+  }
+
+  // Is bear of allowed?
+  if (!player_in_home &&
+      ((to == 26 && player == 0) ||
+       (to == 27 && player == 1)))
+    return false
+
+  // Should bar be moved first?
+  bar_checkers = player == 0 ? this.checkers[24] : this.checkers[25]
+  if ((bar_checkers > 0 && from != 24) ||
+      (bar_checkers < 0 && from != 25))
+    return false
+
+  return true
+}
+
+GameState.prototype.to_pip = function(from, to_move, player) {
+  var to;
+  if (player == 0) {
+    to = from - to_move
+    if (to < 0)
+      to = 26 // Bear off
+  }
+  else {
+    if (from > 23) // From bar
+      to = to_move - 1
+    else
+      to = from + to_move
+
+    if (to > 23)
+      to = 27 // Bear off
+  }
+
+  return to    
+}
+
+GameState.prototype.move_from = function(from) {
+  var to_move = this.to_move()
+  var player = this.on_turn()
+  var checker = this.checkers[from] < 0 ? 1 : 0
+
+  if (checker != player)
+    return null;
+
+  var to = this.to_pip(from, to_move, player);
+
+  if (this.allowed_move(from, to, player) == false)
+    return null
+
+  move = gameState.move(from, to, player)
+  if (move == null)
+    return null
+
+  gameState.moved()
+
+  move.push({from: from, to: to, player: player})
+  return move
+}
+
+GameState.prototype.moved = function() {
+  if (this.dice_moved == this.dice.length)
+    return
+  this.dice_moved += 1
+}
+
+GameState.prototype.on_turn = function() {
+  return this.player
+}
+
+GameState.prototype.end_turn = function() {
+  this.roll()
+  this.change_player()
+}
+
+GameState.prototype.change_player = function() {
+  this.player = this.player == 0 ? 1 : 0
+  return this.player
+}
+
+GameState.prototype.to_move = function() {
+  if (this.dice_moved >= this.dice.length)
+    return -1;
+  return this.dice[this.dice_moved]
+}
+
+GameState.prototype.roll = function() {
+  die1 = Math.floor(Math.random() * 6) + 1
+  die2 = Math.floor(Math.random() * 6) + 1
+
+  this.dice = []
+  this.dice.push(Math.max(die1, die2))
+  this.dice.push(Math.min(die1, die2))
+  
+  if (this.dice[0] > this.dice[1])
+    this.swap_dice()
+
+  if (this.dice[0] == this.dice[1]) {
+    this.dice.push(this.dice[0])
+    this.dice.push(this.dice[0])
+  }
+
+  this.dice_moved = 0
+}
+
+GameState.prototype.swap_dice = function() {
+  if (this.dice_moved != 0 || this.dice[0] == this.dice[1])
+    return
+
+  tmp = this.dice[0]
+  this.dice[0] = this.dice[1]
+  this.dice[1] = tmp
+}
+
+GameState.prototype.move = function(from, to, player) {
+  ret = []
+  if (player == 0) {
+    dir = 1
+    if (this.checkers[from] < 1 || this.checkers[to] < 0) {
+      if (this.checkers[to] == -1) {
+        this.checkers[to] = 0
+        this.checkers[25]--
+
+        ret.push({from: to, to: 25, player: 1})
+      }
+      else {
+        return null
+      }
+    }
+  }
+  else {
+    dir = -1
+    if (this.checkers[from] > 0 || this.checkers[to] > 0) {
+      if (this.checkers[to] == 1) {
+        this.checkers[to] = 0
+        this.checkers[24]++
+
+        ret.push({from: to, to: 24, player: 0})
+      }
+      else {
+        return null
+      }
+    }
+  }
+
+  this.checkers[from] -= dir
+  this.checkers[to] += dir
+
+  return ret
+}
+
+
+
+
