2e1961fd29c905a52290ea0542f7d9fbef21eb58
Miika Lappalainen <miika@foodie.fm>
2017-02-08 14:00:21 +0200

    Use Bluebird promises.

--- config/setup.js
+++ config/setup.js
@@ -1,3 +1,8 @@
 // Globals
 MONGOOSE = require('mongoose');
+MONGOOSE.Promise = require('bluebird')
 MONGOOSE.connect('mongodb://localhost:27017/testapp')
+
+// Plugin Bluebird
+Promise = require("bluebird");
+
